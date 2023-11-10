b2bdb65f53d1ff49b01b7096e7bf03fffaaf8e9c
Daniel Peach <djpeach96@gmail.com>
2023-11-09 21:08:17 -0500

    init

---
+++ src/main/kotlin/com/example/librarydemomedium/LibraryDemoMediumApplication.kt
@@ -0,0 +1,11 @@
+package com.example.librarydemomedium
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryDemoMediumApplication
+
+fun main(args: Array<String>) {
+  runApplication<LibraryDemoMediumApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/librarydemomedium/LibraryDemoMediumApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.librarydemomedium
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryDemoMediumApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
